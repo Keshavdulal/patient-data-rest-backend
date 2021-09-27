1d829e609bd47f84d086e1017679a04234507ed9
keshavdulal <keshav.dulal@gmail.com>
2021-09-27 22:08:25 +0545

    test(app): demo commit

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
-var http = require('http');
+/**
+ * Authors
+ * - Aishwarya Shrestha ()
+ * - Shrijan Karki (1234)
+ * - Justice Ekeke ()
+ * - Basil Dhillo ()
+ * - Keshav Dulal (301209947)
+ */
+
+// Library Imports
+const express = require('express');
    
 const PORT_ADDR = 8000;
 const LOCALHOST = '127.0.0.1'
    
-var server = http.createServer(function (request, response) {
-  
-  // handle different request urls using switch
-  switch (request.url) {
-    case '/':
-      response.writeHead(200, { "Content-Type": "text/plain" }); // HTTP header
-      response.end('Welcome to the Server')
-      break;
+  var server = http.createServer(function (request, response) {
     
-    case '/images':
-      response.writeHead(200, { "Content-Type": "text/plain" }); // HTTP header
-      response.end('Images endpoint')
-      break;
-  }
-});
+    // handle different request urls using switch
+    switch (request.url) {
+      case '/':
+        response.writeHead(200, { "Content-Type": "text/plain" }); // HTTP header
+        response.end('Welcome to the Server')
+        break;
+      
+      case '/images':
+        response.writeHead(200, { "Content-Type": "text/plain" }); // HTTP header
+        response.end('Images endpoint')
+        break;
+    }
+  });
 
 
-server.listen(PORT_ADDR, LOCALHOST, () => {
-  console.log(`Server is running at ${LOCALHOST}:${PORT_ADDR}`);
-  console.log(`Following routes are available: \n/\ \n/\images`)
-});
+  server.listen(PORT_ADDR, LOCALHOST, () => {
+    console.log(`Server is running at ${LOCALHOST}:${PORT_ADDR}`);
+    console.log(`Following routes are available: \n/\ \n/\images`)
+  });
