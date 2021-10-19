4b891f16daed346fc74775fb3372ca69f0087076
keshavdulal <keshav.dulal@gmail.com>
2021-10-19 23:34:00 +0545

    restructure the app layout

--- index.js
+++ index.js
@@ -1,26 +1,45 @@
-var http = require('http');
-   
+const express = require("express"); // core-library to run our server on top of nodejs
+
+// Application Constants
 const PORT_ADDR = 8000;
-const LOCALHOST = '127.0.0.1'
-   
-var server = http.createServer(function (request, response) {
-  
-  // handle different request urls using switch
-  switch (request.url) {
-    case '/':
-      response.writeHead(200, { "Content-Type": "text/plain" }); // HTTP header
-      response.end('Welcome to the Server')
-      break;
-    
-    case '/images':
-      response.writeHead(200, { "Content-Type": "text/plain" }); // HTTP header
-      response.end('Images endpoint')
-      break;
-  }
+const LOCALHOST = "127.0.0.1";
+
+// create an instance of express to run your app or server;
+const server = express();
+
+// parse string back to json
+server.use(express.json());
+
+// Endpoints
+
+// Home endpoint - Request type GET
+server.get("/", (request, response) => {
+  response.json({
+    message: "Welcome to Patient-Data-Rest-Backend.",
+  });
 });
 
+/**
+ * General Information on Patients
+ *  Name, Age, Height, Weight, Address, Contact No, Emergency Contact No, Blood Group, Remarks
+ */
 
-server.listen(PORT_ADDR, LOCALHOST, () => {
+// TODO: Aishwarya
+server.post("/patient-info", (request, response) => {});
+
+// TODO: Basil
+server.get("/patient-info", (request, response) => {});
+
+// TODO: Shrijan
+server.get("/patient-info-all", (request, response) => {});
+
+// TODO: Justice
+server.post("/patient-test", (request, response) => {});
+
+// TODO: Keshav
+server.get("/patient-test", (request, response) => {});
+
+server.listen(PORT_ADDR, () => {
   console.log(`Server is running at ${LOCALHOST}:${PORT_ADDR}`);
-  console.log(`Following routes are available: \n/\ \n/\images`)
+  console.log(`Following routes are available: \n/\ \n/\images`);
 });
